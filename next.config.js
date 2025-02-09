ade65fbd84d4bccdbe9732eb4a52c2c4d1a45472
Kris <dev01.sosc@carehealth.io>
2025-02-09 21:28:36 +0700

    Ignore ts during build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ["lh3.googleusercontent.com", "firebasestorage.googleapis.com", "res.cloudinary.com"]
-    }
-}
+  images: {
+    domains: [
+      'lh3.googleusercontent.com',
+      'firebasestorage.googleapis.com',
+      'res.cloudinary.com',
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
